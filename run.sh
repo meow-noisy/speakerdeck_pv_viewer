c7dfe0a596177034f1c0acb3859e3301cf7f12c8
meow <meow.noisy.code@gmail.com>
2021-07-28 19:05:24 +0900

    fix getting abs path on MacOS

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
-SCRIPT_DIR_PATH=$(dirname $(readlink -f $0))
-cd $SCRIPT_DIR_PATH
-
+SCRIPT_DIR_PATH=$(cd $(dirname $0); pwd)
+cd "$SCRIPT_DIR_PATH"
 python scrape_speakerdeck_pv.py
 python data_aggregate.py
