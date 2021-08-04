66dec24c9c9245f3427638023d4de0cb1c9800ba
meow <5676007+meow-noisy@users.noreply.github.com>
2021-08-04 21:40:47 +0900

    add python script for lambda

---
+++ lambda.py
@@ -0,0 +1,55 @@
+
+# scrape speakerdeck.com users daily pv and create a csv file.
+
+from pathlib import Path
+
+import csv
+import requests
+from bs4 import BeautifulSoup
+import boto3
+from datetime import datetime
+
+
+s3 = boto3.resource('s3')
+
+
+def lambda_handler(event, context):
+
+    bucket = 'YOUR S3 BUCKET'
+    url = "https://speakerdeck.com/ your speakerdeck account"
+
+    r = requests.get(url)
+
+    html = r.text
+    soup = BeautifulSoup(html, "lxml")
+
+    container = soup.select(".container.py-md-4.py-3")[0]
+    slides = container.select(
+        '.row.mb-4')[0].select(".col-12.col-md-6.col-lg-4.mb-5")
+
+    l = []
+    for slide in slides:
+        slide_id = slide.select(".card.deck-preview")[0]["data-id"]
+        slide_title = slide.find('a')["title"]
+
+        elem = slide.find('div')
+        elem.select('.row.mb-4')
+        span = elem.find("span")
+        pv = span["title"].replace(" views", "")
+        l.append([slide_id, slide_title, pv])
+
+    dt_now = datetime.now()
+    str_dt_now = dt_now.strftime('%Y-%m-%d')
+    print(str_dt_now)
+    p = Path(f"result/{str_dt_now}.tsv")
+    p.parent.mkdir(exist_ok=True, parents=True)
+
+    with p.open('w') as f:
+        writer = csv.writer(f, delimiter='\t')
+        for record in l:
+            writer.writerow(record)
+    key = str(p)
+
+    obj = s3.Object(bucket, key)
+    obj.upload_file(str(p))
+    return
